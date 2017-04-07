c05779b4db36c7bd56bf03e699c3e4ad6c3d96bf
geniusgeek <geniusgeek2014@gmail.com>
2017-04-07 14:38:10 +0100

    made some necessary classes final

--- trupple-mvp/src/main/java/com/github/geniusgeek/trupple_mvp/exceptions/AuthenticationException.java
+++ trupple-mvp/src/main/java/com/github/geniusgeek/trupple_mvp/exceptions/AuthenticationException.java
@@ -3,7 +3,7 @@ package com.github.geniusgeek.trupple_mvp.exceptions;
 /** An exception to flag due to failed authentication
  * Created by Genius on 28/04/2016.
  */
-public final class AuthenticationException extends Exception {
+public  class AuthenticationException extends Exception {
     public AuthenticationException(String msg, Throwable t) {
         super(msg, t);
     }
